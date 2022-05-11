b8a65f0aaf92ce0dee43365b860c402eb2766ca9
EmanuelVM2002 <emanuelvillada10@gmail.com>
2022-05-11 15:39:11 -0500

    123

--- Alaska.Web/Models/Order.cs
+++ Alaska.Web/Models/Order.cs
@@ -5,7 +5,7 @@ namespace Alaska.Web.Models
     public class Order
     {
         public int Id { get; set; } 
-        public string Detalle { get; set; }
+        public string Detalles { get; set; }
         public int EstadoPedido { get; set; }
     }
 }
