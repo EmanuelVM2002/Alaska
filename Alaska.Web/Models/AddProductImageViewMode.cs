3939d911b49f6b63c335723e226e7738dc19c4ed
EMANUEL VILLADA MU�ETON <emanuelvillada302298@correo.itm.edu.co>
2022-05-19 13:03:23 -0500

    Guia 2 Hecha

---
+++ Alaska.Web/Models/AddProductImageViewMode.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Alaska.Web.Models
+{
+    public class AddProductImageViewMode
+    {
+        public int ProductId { get; set; }
+
+        [Display(Name = "Image")]
+        [Required]
+        public IFormFile ImageFile { get; set; }
+
+    }
+}
